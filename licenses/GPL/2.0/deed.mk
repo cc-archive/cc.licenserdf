fe6546d4aa9fc415314b84c18298a917c546e08b
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-11-01 20:20:35 +0000

    Adding static deeds.
    git-svn-id: http://code.creativecommons.org/svnroot/license.rdf/trunk@7836 aec4f576-4910-0410-beeb-ecb0427469e7

---
+++ licenses/GPL/2.0/deed.mk
@@ -0,0 +1,381 @@
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:cc="http://creativecommons.org/ns#">
+  <head>
+<base href="http://localhost:8080/licenses/GPL/2.0/deed/@@index" />
+
+    <title>Creative Commons 
+    GNU General Public License
+  </title><link rel="stylesheet" type="text/css"
+                href="http://localhost:8080/licenses/@@/cc/includes/deed3.css"
+                media="screen" />
+    
+    <link rel="stylesheet" type="text/css"
+          href="http://localhost:8080/licenses/@@/cc/includes/deed3-print.css"
+          media="print" />
+
+    <!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="/includes/deed3-ie.css" media="screen" tal:attributes="href context/++resource++cc/includes/deed3-ie.css" /><![endif]-->
+
+    <link rel="alternate" type="application/rdf+xml" href="rdf" /> 
+    
+
+<script type="text/javascript">
+function setCookie(name, value, expires, path, domain, secure) {
+    document.cookie= name + "=" + escape(value) +
+        ((expires) ? "; expires=" + expires.toGMTString() : "") +
+        ((path) ? "; path=" + path : "") +
+        ((domain) ? "; domain=" + domain : "") +
+        ((secure) ? "; secure" : "");
+}
+var expiry = new Date();
+expiry.setTime(expiry.getTime()+(5*365*24*60*60*1000));
+setCookie('lang','%s', expiry, '/');
+</script>
+
+
+    <script type="text/javascript"
+            src="http://localhost:8080/licenses/@@/cc/includes/referrer/ccdeed.js">
+    </script>
+    <script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        urchin = new urchin();
+        _uacct="UA-2010376-1";  urchinTracker();
+    </script>
+
+   
+
+  </head>
+  <body onload="referrerMetadata()">
+
+    
+    
+
+    <div id="header">
+    <p align="center"><a href="/">Криејтив комонс</a></p>
+
+    
+
+<div style="width: 620px; margin-left: auto; margin-right: auto; text-align: center;">
+<span align="left" dir="">Оваа страница е достапна на следните јазици</span>
+<br />
+
+
+
+<a href="./deed.af" title="Afrikaans" hreflang="af"
+   rel="alternate nofollow" lang="af">Afrikaans</a>
+
+
+
+
+<a href="./deed.bg" title="български" hreflang="bg"
+   rel="alternate nofollow" lang="bg">български</a>
+
+
+
+
+<a href="./deed.ca" title="Català" hreflang="ca"
+   rel="alternate nofollow" lang="ca">Català</a>
+
+
+
+
+<a href="./deed.da" title="Dansk" hreflang="da"
+   rel="alternate nofollow" lang="da">Dansk</a>
+
+
+
+
+<a href="./deed.de" title="Deutsch" hreflang="de"
+   rel="alternate nofollow" lang="de">Deutsch</a>
+
+
+
+
+<a href="./deed.en" title="English" hreflang="en"
+   rel="alternate nofollow" lang="en">English</a>
+
+
+
+
+<a href="./deed.en_CA" title="English (CA)" hreflang="en_CA"
+   rel="alternate nofollow" lang="en_CA">English (CA)</a>
+
+
+
+
+<a href="./deed.en_GB" title="English (GB)" hreflang="en_GB"
+   rel="alternate nofollow" lang="en_GB">English (GB)</a>
+
+
+
+
+<a href="./deed.en_US" title="English (US)" hreflang="en_US"
+   rel="alternate nofollow" lang="en_US">English (US)</a>
+
+
+
+
+<a href="./deed.eo" title="Esperanto" hreflang="eo"
+   rel="alternate nofollow" lang="eo">Esperanto</a>
+
+
+
+
+<a href="./deed.es" title="Castellano" hreflang="es"
+   rel="alternate nofollow" lang="es">Castellano</a>
+
+
+
+
+<a href="./deed.es_AR" title="Castellano (AR)"
+   hreflang="es_AR" rel="alternate nofollow" lang="es_AR">Castellano (AR)</a>
+
+
+
+
+<a href="./deed.es_CL" title="Español (CL)" hreflang="es_CL"
+   rel="alternate nofollow" lang="es_CL">Español (CL)</a>
+
+
+
+
+<a href="./deed.es_CO" title="Spanish (CO)" hreflang="es_CO"
+   rel="alternate nofollow" lang="es_CO">Spanish (CO)</a>
+
+
+
+
+<a href="./deed.es_MX" title="Castellano (MX)"
+   hreflang="es_MX" rel="alternate nofollow" lang="es_MX">Castellano (MX)</a>
+
+
+
+
+<a href="./deed.es_PE" title="Castellano (PE)"
+   hreflang="es_PE" rel="alternate nofollow" lang="es_PE">Castellano (PE)</a>
+
+
+
+
+<a href="./deed.eu" title="Euskara" hreflang="eu"
+   rel="alternate nofollow" lang="eu">Euskara</a>
+
+
+
+
+<a href="./deed.fi" title="Suomeksi" hreflang="fi"
+   rel="alternate nofollow" lang="fi">Suomeksi</a>
+
+
+
+
+<a href="./deed.fr" title="français" hreflang="fr"
+   rel="alternate nofollow" lang="fr">français</a>
+
+
+
+
+<a href="./deed.fr_CA" title="français (CA)"
+   hreflang="fr_CA" rel="alternate nofollow" lang="fr_CA">français (CA)</a>
+
+
+
+
+<a href="./deed.gl" title="Galego" hreflang="gl"
+   rel="alternate nofollow" lang="gl">Galego</a>
+
+
+
+
+<a href="./deed.he" title="עברית" hreflang="he"
+   rel="alternate nofollow" lang="he">עברית</a>
+
+
+
+
+<a href="./deed.hr" title="hrvatski" hreflang="hr"
+   rel="alternate nofollow" lang="hr">hrvatski</a>
+
+
+
+
+<a href="./deed.hu" title="Magyar" hreflang="hu"
+   rel="alternate nofollow" lang="hu">Magyar</a>
+
+
+
+
+<a href="./deed.it" title="Italiano" hreflang="it"
+   rel="alternate nofollow" lang="it">Italiano</a>
+
+
+
+
+<a href="./deed.ja" title="日本語" hreflang="ja"
+   rel="alternate nofollow" lang="ja">日本語</a>
+
+
+
+
+<a href="./deed.ko" title="한국어" hreflang="ko"
+   rel="alternate nofollow" lang="ko">한국어</a>
+
+
+
+
+<a href="./deed.mk" title="Macedonian" hreflang="mk"
+   rel="alternate nofollow" lang="mk">Macedonian</a>
+
+
+
+
+<a href="./deed.ms" title="Melayu" hreflang="ms"
+   rel="alternate nofollow" lang="ms">Melayu</a>
+
+
+
+
+<a href="./deed.nl" title="Nederlands" hreflang="nl"
+   rel="alternate nofollow" lang="nl">Nederlands</a>
+
+
+
+
+<a href="./deed.nso" title="Sesotho sa Leboa" hreflang="nso"
+   rel="alternate nofollow" lang="nso">Sesotho sa Leboa</a>
+
+
+
+
+<a href="./deed.pl" title="polski" hreflang="pl"
+   rel="alternate nofollow" lang="pl">polski</a>
+
+
+
+
+<a href="./deed.pt" title="Português" hreflang="pt"
+   rel="alternate nofollow" lang="pt">Português</a>
+
+
+
+
+<a href="./deed.sl" title="slovenski jezik" hreflang="sl"
+   rel="alternate nofollow" lang="sl">slovenski jezik</a>
+
+
+
+
+<a href="./deed.st" title="Sotho" hreflang="st"
+   rel="alternate nofollow" lang="st">Sotho</a>
+
+
+
+
+<a href="./deed.sv" title="svenska" hreflang="sv"
+   rel="alternate nofollow" lang="sv">svenska</a>
+
+
+
+
+<a href="./deed.zh" title="简体中文" hreflang="zh"
+   rel="alternate nofollow" lang="zh">简体中文</a>
+
+
+
+
+<a href="./deed.zh_TW" title="華語 (台灣)" hreflang="zh_TW"
+   rel="alternate nofollow" lang="zh_TW">華語 (台灣)</a>
+
+
+
+
+<a href="./deed.zu" title="isiZulu" hreflang="zu"
+   rel="alternate nofollow" lang="zu">isiZulu</a>
+
+
+</div>
+
+
+    </div>
+
+    <div id="deed" align="left" class="green" dir="">  
+      <div id="deed-head">
+	      <div id="cc-logo"><img src="/images/deed/cc-logo.jpg" alt="cc logo" /></div>
+	      <h1><span>Creative Commons License Deed</span></h1>
+        <div id="deed-license">
+	        <h2>GNU General Public License, <a href="http://www.gnu.org/">Фондација за слободен софтвер</a></h2>
+        </div>
+      </div>
+
+      <div id="deed-main" align="left" dir="">
+       <div id="deed-main-content">
+          <div>
+
+<p>Лиценцата <span>GNU General Public License</span> е <a href="http://www.gnu.org/philosophy/free-sw.html">Free Software</a> лиценца за Слободен софтвер. Како и секоја друга лиценца за слободен софтвер ви ги дава следните четири слободи:</p>
+			
+			<ol start="0">
+				<li>Слобода да ја користите програмата за каква било цел.</li>
+				<li>Слобода да истражите како работи програмата и да ја адаптирате на вашите потреби</li>
+				<li>Слобода да дистрибуирате копии за да му помогнете на соседот.</li>
+				<li>Слобода да ја подобрувате програмата и јавно да ги објавите подобрувањата, за целата заедница да има корист.</li>
+			</ol>
+
+   <p />Овие слободи можете да ги искористите само ако се придржувате до изричните услови наведени во оваа лиценца. Основните услови се:
+   
+	        <div align="center"><table class="draft" width="500" cellpadding="4" cellspacing="0" border="0">
+		<tbody>    
+			<tr>
+				<td><img hspace="4" align="left" alt="!" src="/icons/nt/deed.gif" border="0">
+				</td><td align="left"><div class="tiny">На секоја дистрибуирана копија морате јасно и соодветно да објавите предупредување за авторско право и забелешка за отсуство на гаранција и да ги оставите непроменети сите предупредувања кои се однесуваат на оваа лиценца и на отсуство на гаранција, а на секој корисник на програмата треба да му дадете копија од <span>GNU General Public License</span> заедно со програмата. Секој превод на <span>GNU General Public License</span> мора да биде проследен со <span>GNU General Public License</span>.</div></td>
+			</tr>
+			<tr>
+				<td><img hspace="4" align="left" alt="sa" src="/icons/sa/deed.gif" border="0">
+				</td><td align="left"><div class="tiny">Ако ја модифицирате вашата копија и копии од програмата или некој нејзин дел, или развивате програма заснована врз неа, вие можете да го дистрибуирате делото што ќе го добиете под <span>GNU General Public License</span>. Секој превод на <span>GNU General Public License</span> мора да биде проследен со <span>GNU General Public License</span>.</div></td>
+			</tr>
+			<tr>
+				<td><img hspace="4" align="left" alt="sc" src="/icons/sc/deed.gif" border="0">
+				</td><td align="left"><div class="tiny">Ако ја копирате или дистрибуирате програмата мора да биде проследена со целосен соодветен машински-читлив изворен код или со писмена понуда, валидна најмалку три години, дека ќе ставите на располагање машински-читлив изворен код.</div></td>
+			</tr>
+		</tbody>
+		</table>
+		</div>
+
+<p>Секој од горенаведените услови може да биде поништен ако добиете дозвола од носителот на авторските права.</p> 
+<p>Горенаведеното на ниту еден начин не влијае врз законските ограничувања на авторските права.</p> 
+<p />Ова е разбирливо резиме на Правниот код (<a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html">целосна ГНУ пшта јавна лиценца)</a>).
+
+
+<p><div align="center"><a href="/licenses/disclaimer-popup" onclick="window.open('/licenses/disclaimer-popup', 'characteristic_help', 'width=375,height=300,scrollbars=yes,resizable=yes,toolbar=no,directories=no,location=yes,menubar=no,status=yes');return false;">Напомена</a> <a href="/licenses/disclaimer-popup" onclick="window.open('/licenses/disclaimer-popup', 'characteristic_help', 'width=375,height=300,scrollbars=yes,resizable=yes,toolbar=no,directories=no,location=yes,menubar=no,status=yes');return false;"><img src="/images/popup.gif" width="15" height="13" alt="disclaimer" border="0"></a></div></p> 
+
+</div>
+       </div>
+      </div>
+
+
+       <div id="deed-foot">
+          
+
+        
+			  <p id="disclaimer">
+          <a href="/licenses/disclaimer-popup"
+             onclick="window.open('/licenses/disclaimer-popup', 'characteristic_help', 'width=375,height=300,scrollbars=yes,resizable=yes,toolbar=no,directories=no,location=yes,menubar=no,status=yes'); return false;">Забелешка</a> 
+          <a href="/licenses/disclaimer-popup"
+             onclick="window.open('/licenses/disclaimer-popup', 'characteristic_help', 'width=375,height=300,scrollbars=yes,resizable=yes,toolbar=no,directories=no,location=yes,menubar=no,status=yes'); return false;">
+          <img src="/images/popup.gif" width="15" height="13" alt="disclaimer" border="0" /></a>
+  		  </p>
+        <p align="center" style="margin-top:40px"><strong>Горенаведеното на ниту еден начин не влијае врз законските ограничувања на авторските права.</strong></p>
+  <p align="center">
+
+    
+
+    Ова е читливо резиме на <a href="legalcode" class="fulltext"> Правниот код (целосната лиценца).
+ </p>
+
+         
+       </div>
+    </div>
+    <p id="footer">
+      <a href="/about/licenses">Научете како да го дистрибуирате вашето дело под оваа лиценца</a>
+    </p>
+
+  </body>
+</html>
---
+++ licenses/nc/2.0/jp/deed.sv
@@ -0,0 +1,400 @@
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:cc="http://creativecommons.org/ns#">
+  <head>
+<base href="http://localhost:8080/licenses/nc/2.0/jp/deed/@@index" />
+
+    <title>Creative Commons 
+    Noncommercial 2.0 Japan
+  </title><link rel="stylesheet" type="text/css"
+                href="http://localhost:8080/licenses/@@/cc/includes/deed3.css"
+                media="screen" />
+    
+    <link rel="stylesheet" type="text/css"
+          href="http://localhost:8080/licenses/@@/cc/includes/deed3-print.css"
+          media="print" />
+
+    <!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="/includes/deed3-ie.css" media="screen" tal:attributes="href context/++resource++cc/includes/deed3-ie.css" /><![endif]-->
+
+    <link rel="alternate" type="application/rdf+xml" href="rdf" /> 
+    
+
+<script type="text/javascript">
+function setCookie(name, value, expires, path, domain, secure) {
+    document.cookie= name + "=" + escape(value) +
+        ((expires) ? "; expires=" + expires.toGMTString() : "") +
+        ((path) ? "; path=" + path : "") +
+        ((domain) ? "; domain=" + domain : "") +
+        ((secure) ? "; secure" : "");
+}
+var expiry = new Date();
+expiry.setTime(expiry.getTime()+(5*365*24*60*60*1000));
+setCookie('lang','%s', expiry, '/');
+</script>
+
+
+    <script type="text/javascript"
+            src="http://localhost:8080/licenses/@@/cc/includes/referrer/ccdeed.js">
+    </script>
+    <script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        urchin = new urchin();
+        _uacct="UA-2010376-1";  urchinTracker();
+    </script>
+
+   
+
+  </head>
+  <body onload="referrerMetadata()">
+
+    <!-- 
+<rdf:RDF xmlns="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+      <License rdf:about="http://creativecommons.org/licenses/nc/2.0/jp/">
+        <permits rdf:resource="http://creativecommons.org/ns#Reproduction"/>
+        <permits rdf:resource="http://creativecommons.org/ns#Distribution"/>
+        <requires rdf:resource="http://creativecommons.org/ns#Notice"/>
+        <prohibits rdf:resource="http://creativecommons.org/ns#CommercialUse"/>
+        <permits rdf:resource="http://creativecommons.org/ns#DerivativeWorks"/>
+      </License>
+    </rdf:RDF>
+   -->
+
+    <div id="header">
+    <p align="center"><a href="/">Creative Commons</a></p>
+
+    
+
+<div style="width: 620px; margin-left: auto; margin-right: auto; text-align: center;">
+<span align="left" dir="">Denna sida finns tillgänglig på följande språk:</span>
+<br />
+
+
+
+<a href="./deed.af" title="Afrikaans" hreflang="af"
+   rel="alternate nofollow" lang="af">Afrikaans</a>
+
+
+
+
+<a href="./deed.bg" title="български" hreflang="bg"
+   rel="alternate nofollow" lang="bg">български</a>
+
+
+
+
+<a href="./deed.ca" title="Català" hreflang="ca"
+   rel="alternate nofollow" lang="ca">Català</a>
+
+
+
+
+<a href="./deed.da" title="Dansk" hreflang="da"
+   rel="alternate nofollow" lang="da">Dansk</a>
+
+
+
+
+<a href="./deed.de" title="Deutsch" hreflang="de"
+   rel="alternate nofollow" lang="de">Deutsch</a>
+
+
+
+
+<a href="./deed.en" title="English" hreflang="en"
+   rel="alternate nofollow" lang="en">English</a>
+
+
+
+
+<a href="./deed.en_CA" title="English (CA)" hreflang="en_CA"
+   rel="alternate nofollow" lang="en_CA">English (CA)</a>
+
+
+
+
+<a href="./deed.en_GB" title="English (GB)" hreflang="en_GB"
+   rel="alternate nofollow" lang="en_GB">English (GB)</a>
+
+
+
+
+<a href="./deed.en_US" title="English (US)" hreflang="en_US"
+   rel="alternate nofollow" lang="en_US">English (US)</a>
+
+
+
+
+<a href="./deed.eo" title="Esperanto" hreflang="eo"
+   rel="alternate nofollow" lang="eo">Esperanto</a>
+
+
+
+
+<a href="./deed.es" title="Castellano" hreflang="es"
+   rel="alternate nofollow" lang="es">Castellano</a>
+
+
+
+
+<a href="./deed.es_AR" title="Castellano (AR)"
+   hreflang="es_AR" rel="alternate nofollow" lang="es_AR">Castellano (AR)</a>
+
+
+
+
+<a href="./deed.es_CL" title="Español (CL)" hreflang="es_CL"
+   rel="alternate nofollow" lang="es_CL">Español (CL)</a>
+
+
+
+
+<a href="./deed.es_CO" title="Spanish (CO)" hreflang="es_CO"
+   rel="alternate nofollow" lang="es_CO">Spanish (CO)</a>
+
+
+
+
+<a href="./deed.es_MX" title="Castellano (MX)"
+   hreflang="es_MX" rel="alternate nofollow" lang="es_MX">Castellano (MX)</a>
+
+
+
+
+<a href="./deed.es_PE" title="Castellano (PE)"
+   hreflang="es_PE" rel="alternate nofollow" lang="es_PE">Castellano (PE)</a>
+
+
+
+
+<a href="./deed.eu" title="Euskara" hreflang="eu"
+   rel="alternate nofollow" lang="eu">Euskara</a>
+
+
+
+
+<a href="./deed.fi" title="Suomeksi" hreflang="fi"
+   rel="alternate nofollow" lang="fi">Suomeksi</a>
+
+
+
+
+<a href="./deed.fr" title="français" hreflang="fr"
+   rel="alternate nofollow" lang="fr">français</a>
+
+
+
+
+<a href="./deed.fr_CA" title="français (CA)"
+   hreflang="fr_CA" rel="alternate nofollow" lang="fr_CA">français (CA)</a>
+
+
+
+
+<a href="./deed.gl" title="Galego" hreflang="gl"
+   rel="alternate nofollow" lang="gl">Galego</a>
+
+
+
+
+<a href="./deed.he" title="עברית" hreflang="he"
+   rel="alternate nofollow" lang="he">עברית</a>
+
+
+
+
+<a href="./deed.hr" title="hrvatski" hreflang="hr"
+   rel="alternate nofollow" lang="hr">hrvatski</a>
+
+
+
+
+<a href="./deed.hu" title="Magyar" hreflang="hu"
+   rel="alternate nofollow" lang="hu">Magyar</a>
+
+
+
+
+<a href="./deed.it" title="Italiano" hreflang="it"
+   rel="alternate nofollow" lang="it">Italiano</a>
+
+
+
+
+<a href="./deed.ja" title="日本語" hreflang="ja"
+   rel="alternate nofollow" lang="ja">日本語</a>
+
+
+
+
+<a href="./deed.ko" title="한국어" hreflang="ko"
+   rel="alternate nofollow" lang="ko">한국어</a>
+
+
+
+
+<a href="./deed.mk" title="Macedonian" hreflang="mk"
+   rel="alternate nofollow" lang="mk">Macedonian</a>
+
+
+
+
+<a href="./deed.ms" title="Melayu" hreflang="ms"
+   rel="alternate nofollow" lang="ms">Melayu</a>
+
+
+
+
+<a href="./deed.nl" title="Nederlands" hreflang="nl"
+   rel="alternate nofollow" lang="nl">Nederlands</a>
+
+
+
+
+<a href="./deed.nso" title="Sesotho sa Leboa" hreflang="nso"
+   rel="alternate nofollow" lang="nso">Sesotho sa Leboa</a>
+
+
+
+
+<a href="./deed.pl" title="polski" hreflang="pl"
+   rel="alternate nofollow" lang="pl">polski</a>
+
+
+
+
+<a href="./deed.pt" title="Português" hreflang="pt"
+   rel="alternate nofollow" lang="pt">Português</a>
+
+
+
+
+<a href="./deed.sl" title="slovenski jezik" hreflang="sl"
+   rel="alternate nofollow" lang="sl">slovenski jezik</a>
+
+
+
+
+<a href="./deed.st" title="Sotho" hreflang="st"
+   rel="alternate nofollow" lang="st">Sotho</a>
+
+
+
+
+<a href="./deed.sv" title="svenska" hreflang="sv"
+   rel="alternate nofollow" lang="sv">svenska</a>
+
+
+
+
+<a href="./deed.zh" title="简体中文" hreflang="zh"
+   rel="alternate nofollow" lang="zh">简体中文</a>
+
+
+
+
+<a href="./deed.zh_TW" title="華語 (台灣)" hreflang="zh_TW"
+   rel="alternate nofollow" lang="zh_TW">華語 (台灣)</a>
+
+
+
+
+<a href="./deed.zu" title="isiZulu" hreflang="zu"
+   rel="alternate nofollow" lang="zu">isiZulu</a>
+
+
+</div>
+
+
+    </div>
+
+    <div id="deed" align="left" class="yellow" dir="">  
+      <div id="deed-head">
+	      <div id="cc-logo"><img src="/images/deed/cc-logo.jpg" alt="cc logo" /></div>
+	      <h1><span>Creative Commons License Deed</span></h1>
+        <div id="deed-license">
+	        <h2>
+Ickekommersiell 2.0 Japan
+
+</h2>
+        </div>
+      </div>
+
+      <div id="deed-main" align="left" dir="">
+       <div id="deed-main-content">
+          
+        
+
+        <div id="deed-rights" align="left" dir="">
+<a href="/worldwide/jp/"><img
+    src="/images/international/jp.png" border="0" /></a>
+<h3 rel="cc:permits"
+    href="http://creativecommons.org/ns#Reproduction">Du får:</h3>
+		      <ul>
+			<li class="license share" rel="cc:permits"
+       href="http://creativecommons.org/ns#Distribution"><strong>att Dela</strong> — att kopiera, distribuera och sända verket</li>
+			      <li class="license remix" rel="cc:permits"
+             href="http://creativecommons.org/ns#DerivativeWorks"><strong>att Remixa</strong> — att skapa bearbetningar</li>
+<span id="devnations-container" />
+		      </ul>
+        </div>
+        <div id="deed-conditions">
+		      <h3>På följande villkor:</h3>
+	        <ul align="left" dir=""> 
+            <li rel="cc:prohibits"
+                href="http://creativecommons.org/ns#CommercialUse"
+                class="license nc-jp">
+	            <p><strong>Noncommercial</strong>. 
+              
+              <span>You may not use this work for commercial purposes.</span>
+              <span id="nc-jp-more-container" /></p>
+            </li>
+            <li id="more-container" class="license-hidden" />
+            <li rel="cc:requires"
+                href="http://creativecommons.org/ns#Notice">Vid all återanvändning och distribution måste du informera om licensvillkoren som gäller för verket. Det bästa sättet att göra detta är genom en länk till den här webbsidan.</li>
+            <li>Undantag från villkoren ovan kan meddelas av upphovsrättsinnehavaren.</li>
+            
+            <li>Ingenting i denna licens begränsar upphovsmannens ideella rätt.</li>
+            
+            
+          </ul>
+
+          
+
+          </div>
+    <span id="referrer-metadata-container" />
+
+       </div>
+      </div>
+
+
+       <div id="deed-foot">
+          
+
+        <p id="disclaimer"><p>
+Commons Deed är inte en licens. Det är endast en enkel sammanfattning för att förstå licenstexten. Det är en lättläst version av några av de viktigaste villkoren. Se det som ett användarvänligt gränssnitt till licenstexten. Commons Deed har ingen juridisk relevans och dess innehåll återfinns inte i licenstexten. Licensvillkoren regleras i licenstexten. <a href="legalcode" class="fulltext">Klicka här för att läsa licenstexten</a>.
+</p>
+
+<p>
+Creative Commons och Creative Commons Japan är inte advokatbyråer eller juridiska byråer. Distribution eller visning av eller länkning till Commons Deed innebär inte ett erbjudande om juridiska tjänster. Creative Commons och Creative Commons Japan är inte parter till licensen som hör till detta Commons Deed och friskriver sig från allt ansvar för skador som kan uppkomma på grund av användning av Commons Deed och tillhörande licens.
+</p>
+
+<p>
+Produkten som licensieras under Creative Commons-licensen tillhandahålls av personen som applicerade licensen. Creative Commons och Creative Commons Japan ger inga garantier eller utfästelser angående licensgivarens status. <a href="http://creativecommons.jp/faq/">Här</a>  finns mer information om applicerande av licensen samt exploatering av produkten som licensierats under Creative Commons-licensen.
+</p></p>
+			  
+        <p align="center" style="margin-top:40px"><strong>Dina lagstadgade rättigheter påverkas inte av denna licens.</strong></p>
+  <p align="center">
+
+    
+
+    Detta är en lättläst sammanfattning av <a href="legalcode" class="fulltext">licenstexten</a>.
+ </p>
+
+         
+       </div>
+    </div>
+    <p id="footer">
+      <a href="/about/licenses">Läs om hur du kan använda denna licens för dina verk</a>
+    </p>
+
+  </body>
+</html>
