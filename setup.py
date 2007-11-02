d7af4de3a0b57d6f706df84f6f99a5cde51af9a4
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-11-02 19:01:37 +0000

    Merge script, build framework for license.rdf tools.
    git-svn-id: http://code.creativecommons.org/svnroot/license.rdf/trunk@7852 aec4f576-4910-0410-beeb-ecb0427469e7

---
+++ setup.py
@@ -0,0 +1,45 @@
+## Copyright (c) 2007 Nathan R. Yergler, Creative Commons
+
+## Permission is hereby granted, free of charge, to any person obtaining
+## a copy of this software and associated documentation files (the "Software"),
+## to deal in the Software without restriction, including without limitation
+## the rights to use, copy, modify, merge, publish, distribute, sublicense,
+## and/or sell copies of the Software, and to permit persons to whom the
+## Software is furnished to do so, subject to the following conditions:
+
+## The above copyright notice and this permission notice shall be included in
+## all copies or substantial portions of the Software.
+
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+## DEALINGS IN THE SOFTWARE.
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "license_rdf.tools",
+    version = "0.1",
+    packages = find_packages('.'),
+    
+    # scripts and dependencies
+    install_requires = ['setuptools',
+                        'rdflib',
+                        ],
+
+
+    entry_points = {'console_scripts' : 
+                    ['merge = tools.merge:cli'],
+                    },
+
+    # author metadata
+    author = 'Nathan R. Yergler',
+    author_email = 'nathan@creativecommons.org',
+    description = 'Tool scripts for manipulating the license RDF files.',
+    license = 'MIT',
+    url = 'http://creativecommons.org',
+
+    )
---
+++ tools/merge.py
@@ -0,0 +1,58 @@
+import os
+import optparse
+
+from rdflib.Graph import Graph
+from rdflib import Namespace, RDF, URIRef, Literal
+
+def create_option_parser():
+    """Return an optparse.OptionParser configured for the merge script."""
+
+    parser = optparse.OptionParser()
+
+    # output options
+    parser.add_option('-o', '--output-file', dest='output_file', 
+                      default='licenses.rdf',
+                      help='Output file for merged RDF.')
+
+    return parser
+
+
+def merge(input_files):
+    """Return a single rdflib Graph containing the contents of input_files.
+    input_files should be a sequence of filenames to load."""
+
+    store = Graph()
+    store.bind("cc", "http://creativecommons.org/ns#")
+    store.bind("dc", "http://purl.org/dc/elements/1.1/")
+    store.bind("dcq","http://purl.org/dc/terms/")
+    store.bind("rdf","http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+
+    for filename in input_files:
+        print 'reading %s...' % filename
+        store.load(filename)
+
+    return store
+
+
+def cli():
+    """Primary command line interface for the merge tool."""
+
+
+    # parser the command line options
+    (options, input_files) = create_option_parser().parse_args()
+
+    # determine the absolute output dir
+    output_fn = os.path.abspath( os.path.join( 
+            os.getcwd(), options.output_file)
+                                  )
+
+    output_file = open(output_fn,"w")
+    output_file.write(
+        merge(input_files).serialize(format="pretty-xml")
+        )
+    output_file.close()
+
+    print 'wrote %s' % output_fn
+
+if __name__ == '__main__':
+    cli()
