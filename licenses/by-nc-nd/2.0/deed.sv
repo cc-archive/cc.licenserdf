7af7998c0254a798502c398cbeca1cfdd3fde92a
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-12-05 20:45:17 +0000

    Merged revisions 8156-8177 via svnmerge from https://cctools.svn.sourceforge.net/svnroot/cctools/license.rdf/trunk
    ........
      r8177 | nyergler | 2007-12-05 11:43:34 -0800 (Wed, 05 Dec 2007) | 2 lines
      
      Regenerated unported/generic deeds, now with correct dc:title markup.
    ........
    
    
    git-svn-id: http://code.creativecommons.org/svnroot/license.rdf/branches/production@8178 aec4f576-4910-0410-beeb-ecb0427469e7

--- licenses/by-nc-nd/2.0/deed.sv
+++ licenses/by-nc-nd/2.0/deed.sv
@@ -65,7 +65,7 @@ setCookie('lang','%s', expiry, '/');
     
 
 <div style="width: 620px; margin-left: auto; margin-right: auto; text-align: center;">
-<span align="left" dir="">Denna sida finns tillgänglig på följande språk:</span>
+<span align="left" dir="">This page is available in the following languages:</span>
 <br />
 
 
@@ -312,7 +312,7 @@ setCookie('lang','%s', expiry, '/');
 	      <div id="cc-logo"><img src="/images/deed/cc-logo.jpg" alt="cc logo" /></div>
 	      <h1><span>Creative Commons License Deed</span></h1>
         <div id="deed-license">
-	        <h2 rel="dc:title">
+	        <h2 property="dc:title">
 Erkännande-Ickekommersiell-Inga bearbetningar 2.0 Generisk
 
 </h2>
