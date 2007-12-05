44783fb3459d213b5431b2241da7ce2a93d65207
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-12-05 19:43:34 +0000

    Regenerated unported/generic deeds, now with correct dc:title markup.
    git-svn-id: http://code.creativecommons.org/svnroot/license.rdf/trunk@8177 aec4f576-4910-0410-beeb-ecb0427469e7

--- licenses/by-sa/3.0/deed.pl
+++ licenses/by-sa/3.0/deed.pl
@@ -66,7 +66,7 @@ setCookie('lang','%s', expiry, '/');
     
 
 <div style="width: 620px; margin-left: auto; margin-right: auto; text-align: center;">
-<span align="left" dir="">Ta strona dostępna jest w następujących językach:</span>
+<span align="left" dir="">This page is available in the following languages:</span>
 <br />
 
 
@@ -313,7 +313,7 @@ setCookie('lang','%s', expiry, '/');
 	      <div id="cc-logo"><img src="/images/deed/cc-logo.jpg" alt="cc logo" /></div>
 	      <h1><span>Creative Commons License Deed</span></h1>
         <div id="deed-license">
-	        <h2 rel="dc:title">
+	        <h2 property="dc:title">
 Uznanie autorstwa-Na tych samych warunkach 3.0 Unported
 
 </h2>
