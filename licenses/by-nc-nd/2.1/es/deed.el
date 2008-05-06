01e7bb9f6249ae98ab308f829ed60ab849794d0e
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2008-05-06 14:47:45 +0000

    Regenerated all deeds with copy and paste attribution.
    git-svn-id: http://code.creativecommons.org/svnroot/license.rdf/trunk@9765 aec4f576-4910-0410-beeb-ecb0427469e7

--- licenses/by-nc-nd/2.1/es/deed.el
+++ licenses/by-nc-nd/2.1/es/deed.el
@@ -6,7 +6,7 @@
     Attribution-Noncommercial-No Derivative Works 2.1 Spain
   </title>
 
-    <base href="http://creativecommons.org/licenses/by-nc-nd/2.1/es/" /><link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.0/build/container/assets/skins/sam/container.css" /> 
+    <!--<base href="" tal:attributes="href context/license/uri" />--><link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.1/build/container/assets/skins/sam/container.css" /> 
 
     <link rel="stylesheet" type="text/css"
           href="http://creativecommons.org/licenses/@@/cc/includes/deed3.css"
@@ -36,39 +36,24 @@ setCookie('lang','%s', expiry, '/');
 
 
 
+    <script type="text/javascript" src="http://yui.yahooapis.com/2.5.1/build/yahoo-dom-event/yahoo-dom-event.js">
+    </script> 
+    <script type="text/javascript" src="http://yui.yahooapis.com/2.5.1/build/connection/connection-min.js">
+    </script> 
+    <script type="text/javascript" src="http://yui.yahooapis.com/2.5.1/build/json/json-min.js">
+    </script>
+
     <script type="text/javascript"
-            src="http://creativecommons.org/licenses/@@/cc/includes/referrer/ccdeed.js">
+            src="http://creativecommons.org/licenses/@@/cc/includes/referrer/deed.js">
     </script>
-
-    <script type="text/javascript" src="http://yui.yahooapis.com/2.5.0/build/yahoo-dom-event/yahoo-dom-event.js"></script> 
     
-<script type="text/javascript" src="http://yui.yahooapis.com/2.5.0/build/container/container-min.js"></script> 
-    <script type="text/javascript">
-<!--
-function init_disclaimer() {
-   var pnl_disclaimer = new YAHOO.widget.Panel("disclaimer_panel", { 
-       close:true,  
-       visible:false,  
-       draggable:false,
-       width:'300px',
-       context:['disclaimer','br','tl']
-       } ); 
-   pnl_disclaimer.render();
 
-   YAHOO.util.Event.addListener("disclaimer", "click",
-       function(e) {
-           pnl_disclaimer.show();
-           YAHOO.util.Event.stopEvent(e);
-       }
-   );
-
-} // init_disclaimer
-
-YAHOO.util.Event.onDOMReady(init_disclaimer);
-
--->
+    <script type="text/javascript" src="http://yui.yahooapis.com/2.5.1/build/container/container-min.js">
     </script>
 
+    <script type="text/javascript"
+            src="http://creativecommons.org/licenses/@@/cc/includes/deed3.js">
+    </script>
 
     <script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
     <script type="text/javascript">
@@ -78,7 +63,7 @@ YAHOO.util.Event.onDOMReady(init_disclaimer);
    
 
   </head>
-  <body onload="referrerMetadata()" class="yui-skin-sam">
+  <body class="yui-skin-sam">
 
     <!-- 
 <rdf:RDF xmlns="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
@@ -93,12 +78,15 @@ YAHOO.util.Event.onDOMReady(init_disclaimer);
    -->
 
     <div id="header">
-    <p align="center"><a href="/">Creative Commons</a></p>
+    <p align="center">
+      <a rel="dc:creator" href="http://creativecommons.org/">
+	<span property="dc:title">Creative Commons</span>
+    </a></p>
 
     
 
 <div style="width: 620px; margin-left: auto; margin-right: auto; text-align: center;">
-<span align="left" dir="">This page is available in the following languages:</span>
+<span align="left" dir="">Η σελίδα αυτή είναι διαθέσιμη στις ακόλουθες γλώσσες:</span>
 <br />
 
 
@@ -187,8 +175,14 @@ YAHOO.util.Event.onDOMReady(init_disclaimer);
 
 
 
-<a href="./deed.es_CO" title="Spanish (CO)" hreflang="es_CO"
-   rel="alternate nofollow" lang="es_CO">Spanish (CO)</a>
+<a href="./deed.es_CO" title="Castellano (CO)"
+   hreflang="es_CO" rel="alternate nofollow" lang="es_CO">Castellano (CO)</a>
+
+
+
+
+<a href="./deed.es_EC" title="Español (Ecuador)"
+   hreflang="es_EC" rel="alternate nofollow" lang="es_EC">Español (Ecuador)</a>
 
 
 
@@ -307,6 +301,12 @@ YAHOO.util.Event.onDOMReady(init_disclaimer);
 
 
 
+<a href="./deed.ro" title="română" hreflang="ro"
+   rel="alternate nofollow" lang="ro">română</a>
+
+
+
+
 <a href="./deed.sl" title="slovenski jezik" hreflang="sl"
    rel="alternate nofollow" lang="sl">slovenski jezik</a>
 
@@ -338,8 +338,8 @@ YAHOO.util.Event.onDOMReady(init_disclaimer);
 
 
 
-<a href="./deed.zh" title="简体中文" hreflang="zh"
-   rel="alternate nofollow" lang="zh">简体中文</a>
+<a href="./deed.zh" title="中文" hreflang="zh"
+   rel="alternate nofollow" lang="zh">中文</a>
 
 
 
@@ -364,7 +364,7 @@ YAHOO.util.Event.onDOMReady(init_disclaimer);
 	      <div id="cc-logo"><img src="/images/deed/cc-logo.jpg" alt="cc logo" /></div>
 	      <h1><span>Creative Commons License Deed</span></h1>
         <div id="deed-license">
-	        <h2 property="dc:title">Attribution-Noncommercial-No Derivative Works 2.1 Spain</h2>
+	        <h2 property="dc:title">Αναφορά-Μη Εμπορική Χρήση-Όχι Παράγωγα Έργα 2.1 Ισπανία</h2>
         </div>
       </div>
 
@@ -384,36 +384,75 @@ YAHOO.util.Event.onDOMReady(init_disclaimer);
 			<li class="license share" rel="cc:permits"
        href="http://creativecommons.org/ns#Distribution"><strong>η διανομή</strong>: Η αναπαραγωγή, διανομή, παρουσίαση στο κοινό του Έργου</li>
 			      
-<span id="devnations-container" />
+            <li id="more-container" class="license-hidden" />
+	    <span id="devnations-container" />
 		      </ul>
         </div>
         <div id="deed-conditions">
-		      <h3>Υπό τις ακόλουθες προϋποθέσεις:</h3>
-	        <ul align="left" dir=""> 
+	  <h3>Υπό τις ακόλουθες προϋποθέσεις:</h3>
+
+	  <ul align="left" dir=""> 
             <li rel="cc:requires"
                 href="http://creativecommons.org/ns#Attribution"
                 class="license by">
-	            <p><strong>Αναφορά</strong>. 
-              <span id="attribution-container">Θα πρέπει να κάνετε την αναφέρετε 
+	      <p><strong>Αναφορά</strong>. 
+		<span id="attribution-container">Θα πρέπει να κάνετε την αναφέρετε 
 στο έργο με τον τρόπο όπως αυτός έχει οριστεί από το δημιουργό ή το χορηγών την άδεια (αλλά όχι με τρόπο που να υποδηλώνει οτι παρέχουν επίσημη έγκριση σε σας ή για χρήση του έργου από εσάς).</span>
-              
-              <span id="by-more-container" /></p>
+		
+		<span id="by-more-container" />
+	      </p>
+
+	      
+		<p id="work-attribution-container" style="display:none;">
+		  <strong>Attribute this work:</strong><br />
+		  <input id="work-attribution" value="" type="text" readonly onclick="this.select()" onfocus="document.getElementById('work-attribution').select();" />
+		  <input id="license-code" type="hidden"
+           value="CC BY-NC-ND 2.1" />
+		  <input id="license-url" type="hidden"
+           value="http://creativecommons.org/licenses/by-nc-nd/2.1/es/" />
+		<a href="" id="attribution_help">
+		  <img src="http://creativecommons.org/licenses/@@/cc/images/information.png" />
+		</a>
+		</p>
+		<div id="attribution_help_panel">
+		  <div class="hd">
+		    What does "Attribute this work" mean?
+		  </div>
+		  <div class="bd">
+		    The page you came from contained embedded licensing
+		    metadata, including how the creator wishes to be 
+		    attributed for re-use.  You can use the HTML here to
+		    cite the work.  Doing so will also include metadata on
+		    your page so that others can find the original work as
+		    well.
+  		  </div>
+		</div>
+
+	      
+
             </li>
             <li rel="cc:prohibits"
                 href="http://creativecommons.org/ns#CommercialUse"
                 class="license nc-eu">
-	            <p><strong>Μη Εμπορική Χρήση</strong>. 
-              
-              <span>Δε μπορείτε να χρησιμοποιήσετε το έργο για εμπορικούς σκοπούς.</span>
-              <span id="nc-eu-more-container" /></p>
+	      <p><strong>Μη Εμπορική Χρήση</strong>. 
+		
+		<span>Δε μπορείτε να χρησιμοποιήσετε το έργο για εμπορικούς σκοπούς.</span>
+		<span id="nc-eu-more-container" />
+	      </p>
+
+	      
+
             </li>
             <li rel="" href="" class="license nd">
-	            <p><strong>Όχι Παράγωγα Έργα</strong>. 
-              
-              <span>Δε μπορείτε να αλλοιώσετε, να τροποιήσετε ή να δημιουργήσετε πάνω στο έργο αυτό.</span>
-              <span id="nd-more-container" /></p>
+	      <p><strong>Όχι Παράγωγα Έργα</strong>. 
+		
+		<span>Δε μπορείτε να αλλοιώσετε, να τροποιήσετε ή να δημιουργήσετε πάνω στο έργο αυτό.</span>
+		<span id="nd-more-container" />
+	      </p>
+
+	      
+
             </li>
-            <li id="more-container" class="license-hidden" />
             <li rel="cc:requires"
                 href="http://creativecommons.org/ns#Notice">Για κάθε επαναχρησιμοποίηση ή διανομή, πρέπει να καταστήσετε σαφείς στους άλλους τους όρους της άδειας αυτού του Έργου. Ο καλύτερος τρόπος για να πράξετε αυτό είναι να δημιουργήσετε ένα σύνδεσμο με το διαδικτυακό τόπο της παρούσας άδειας.</li>
             <li>Κάθε ένας από τους παραπάνω όρους μπορεί να παρακαμφθεί εάν πάρετε άδεια από το δικαιούχο των δικαιωμάτων πνευματικής ιδιοκτησίας</li>
