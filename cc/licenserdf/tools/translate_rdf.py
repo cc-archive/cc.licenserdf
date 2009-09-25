2be659595de0977c193925b21d9d233fc39cb11e
Christopher Allan Webber <cwebber@dustycloud.org>
2009-09-25 11:36:30 -0500

    Okay, translate_rdf works properly now.

--- cc/licenserdf/tools/translate_rdf.py
+++ cc/licenserdf/tools/translate_rdf.py
@@ -3,12 +3,17 @@ Take an rdf file and update any translations that might be available
 for translating.
 """
 
+import os
+
 from argparse import ArgumentParser
+
+from support import *
 
 def get_args():
     """Get all args taken by this app"""
     parser = ArgumentParser(
-        help="Take an rdf file and run it through the translation machinery.")
+        description=(
+            "Take an rdf file and run it through the translation machinery."))
 
     parser.add_argument('rdf_file', nargs=1)
     return parser.parse_args()
@@ -16,7 +21,7 @@ def get_args():
 
 def cli():
     opts = get_args()
-    filename = os.path.abspath(filename)
+    filename = os.path.abspath(opts.rdf_file[0])
     if not os.path.exists(filename):
         print "That filename does not exist."
     graph = load_graph(filename)
