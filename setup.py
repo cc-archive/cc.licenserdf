9a215041ef638011ba45ca18ed7d820ab28260b8
Christopher Allan Webber <cwebber@dustycloud.org>
2009-10-21 17:06:46 -0500

    Updating the version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
