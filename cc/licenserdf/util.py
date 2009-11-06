078146b86d89c3891a65a30efc1cc6ac70c34be8
Christopher Allan Webber <cwebber@dustycloud.org>
2009-11-06 13:29:46 -0600

    Fixing an issue with unicode while doing translations

--- cc/licenserdf/util.py
+++ cc/licenserdf/util.py
@@ -18,14 +18,36 @@ def inverse_translate(string, target_language, domain='cc_org'):
        ...    target_language='en_US')
        >>> translated_string == 'foo Portugal baz GNU General Public License'
     """
-    translated_string = ''
+    string = unicode_cleaner(string)
+    translated_string = u''
     last_pos = 0
     for match in TRANSLATION_BIT_RE.finditer(string):
         translated_string += string[last_pos:match.start()]
         msgid = match.group(1)
         translation = translate(
             msgid, domain=domain, target_language=target_language)
-        translated_string += translation
+        translated_string += unicode_cleaner(translation)
         last_pos = match.end()
     translated_string += string[last_pos:]
     return translated_string
+
+
+def unicode_cleaner(string):
+    if isinstance(string, unicode):
+        return string
+
+    try:
+        return string.decode('utf-8')
+    except UnicodeError:
+        try:
+            return string.decode('latin-1')
+        except UnicodeError:
+            return string.decode('utf-8', 'ignore')
+
+
+def rdf_style_lang(cc_style_lang):
+    lang_parts = cc_style_lang.lower().split('_', 1)
+    if len(lang_parts) == 2:
+        return '%s-%s' % (lang_parts[0], lang_parts[1])
+    else:
+        return lang_parts[0].lower()
