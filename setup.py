19e1d3a833a587b10cc476e00185cd4b57703460
JED3 <jed@jedpad.(none)>
2010-03-17 14:14:23 -0700

    Ecuador jurisidiction was missing labels in the rdf, re-adding ec jurisdiction and incr the package version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.1.9",
+    version = "0.1.10",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
