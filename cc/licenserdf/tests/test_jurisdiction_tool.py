b13464850da48e1af24d9dd4821748de396262ee
Christopher Allan Webber <cwebber@dustycloud.org>
2010-03-25 17:09:00 -0500

    Working on the add tool test

--- cc/licenserdf/tests/test_jurisdiction_tool.py
+++ cc/licenserdf/tests/test_jurisdiction_tool.py
@@ -83,3 +83,47 @@ def test_launch():
 
     # make sure that we got the right save path
     assert graph_saver.save_path == opts.rdf_file
+
+
+def test_add():
+    opts = MockOpts()
+    opts.rdf_file = pkg_resources.resource_filename(
+        'cc.licenserdf.tests', 'rdf/jurisdictions.rdf')
+    opts.jurisdiction = ['it']
+    opts.i18n_dir = pkg_resources.resource_filename(
+        'cc.i18npkg', 'i18n/i18n/')
+    opts.juris_uri = "http://www.creativecommons.it"
+    opts.langs = 'en_US,sr_LATN'
+
+    graph_saver = MockSaveGraph()
+
+    jurisdiction.add(opts, __save_graph=graph_saver)
+
+    # make sure that we got the right save path
+    assert graph_saver.save_path == opts.rdf_file
+
+    graph = graph_saver.graph
+
+    # ensure the jurisdiction exists in the output
+    ## using a list comprehension here because the generators rdflib
+    ## has seem to work weird and aren't list()'able?
+    assert rdflib.URIRef('http://creativecommons.org/international/it/') \
+        in [i for i in graph_saver.graph.subjects()]
+
+    # ensure that launched is set to False
+    result = graph_saver.graph.value(
+        subject=rdflib.URIRef('http://creativecommons.org/international/it/'),
+        predicate=rdflib.URIRef('http://creativecommons.org/ns#launched'))
+    expected_result = rdflib.Literal(
+        u'false',
+        datatype=rdflib.URIRef(
+            'http://www.w3.org/2001/XMLSchema-datatypes#boolean'))
+    assert result == expected_result
+
+    # ensure that jurisdictionSite == opts.juris_uri
+
+    # ensure we have the generated i18n string
+
+    # ensure that the translations were run
+
+    # check that the languages were added
