6ed434e75160eb02df09e7a4058ba14a3b32bc5a
Christopher Allan Webber <cwebber@dustycloud.org>
2010-05-03 15:26:56 -0500

    Ran everything through the translation machinery and making a new package

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.1.14",
+    version = "0.1.15",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
