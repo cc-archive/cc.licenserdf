6a7d0d8fd2c9999829ff55897f8bf8e003705ea9
Christopher Allan Webber <cwebber@dustycloud.org>
2010-05-20 09:57:43 -0500

    New version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.1.17",
+    version = "0.1.18",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
