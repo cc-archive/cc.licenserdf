1119ea2c6dfaf25fef5f967a7c60c85ecf9ec422
Nathan Kinkade <nkinkade@creativecommons.org>
2011-01-05 13:37:06 -0500

    Bumped package version number after launching Costa Rica.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.2.10",
+    version = "0.2.11",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
