872674d944295d6b5972373dbfdf37cd6307bdd7
Nathan Kinkade <nkinkade@creativecommons.org>
2011-05-06 17:09:52 -0400

    Incremented version number of for sdist.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.2.17",
+    version = "0.2.18",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
