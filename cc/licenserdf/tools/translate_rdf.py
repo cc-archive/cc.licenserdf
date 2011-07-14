7abe2d1105adf5c5e66725cb934cb9b3962b1c3e
Jonathan Palecek <jonathan@creativecommons.org>
2011-07-14 11:25:18 -0500

    Adjusted the translation tool to allow for multiple files, and an --all argument

--- cc/licenserdf/tools/translate_rdf.py
+++ cc/licenserdf/tools/translate_rdf.py
@@ -1,9 +1,11 @@
+
 """
 Take an rdf file and update any translations that might be available
 for translating.
 """
 
 import os
+import glob
 
 from argparse import ArgumentParser
 
@@ -15,16 +17,35 @@ def get_args():
         description=(
             "Take an rdf file and run it through the translation machinery."))
 
-    parser.add_argument('rdf_file', nargs=1)
+    parser.add_argument(
+        '-a',
+        '--all',
+        action='store_const',
+        const=True,
+        help="implies: translate_rdf cc/licenserdf/licenses/*.rdf")
+
+    parser.add_argument(
+        'rdf_file',
+        nargs='*')
     return parser.parse_args()
 
 
 def cli():
     opts = get_args()
-    filename = os.path.abspath(opts.rdf_file[0])
-    if not os.path.exists(filename):
-        print "That filename does not exist."
-    graph = load_graph(filename)
-    translate_graph(graph)
-    save_graph(graph, filename)
-    
+    if opts.all:
+        opts.rdf_file = glob.glob("cc/licenserdf/licenses/*.rdf")
+
+    if opts.rdf_file:
+        count = 0
+        for path in opts.rdf_file:
+            if not os.path.exists(path):
+                print "That filename does not exist."
+                exit(1)
+            else:
+                graph = load_graph(path)
+                translate_graph(graph)
+                save_graph(graph, path)
+                count += 1
+        print "Translated", count, "file(s)."
+    else:
+        print "You need to pass at least one argument."
