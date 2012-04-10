a35d89291a6718c631454bb6e43f0ba4f07bf8ca
Nathan Kinkade <nkinkade@creativecommons.org>
2012-04-10 18:49:46 -0400

    Bumped version number by one minor.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.2.31",
+    version = "0.2.33",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
