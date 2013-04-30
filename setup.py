ea09bcca95fcf7d71ac2e39bee36e0306f14cbbc
Nathan Kinkade <nkinkade@creativecommons.org>
2013-04-30 12:51:36 -0400

    Bumped sdist version again, this time after adding RDF and launching jurisdiction.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.2.37",
+    version = "0.2.38",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
