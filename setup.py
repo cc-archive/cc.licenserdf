696e09e497aeae394d1d661dd8528aed3d6995a1
Dan Mills <code@sandmill.org>
2013-09-15 17:30:19 -0700

    launch IGO port; bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cc.licenserdf",
-    version = "0.2.42",
+    version = "0.2.43",
     packages = find_packages('.'),
     namespace_packages = ['cc',],
     
